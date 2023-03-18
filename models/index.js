1d8b5299cd63c2c341f1928b6b6aa9a312c932fb
Віталій Гоцуляк <shuricks007@gmail.com>
2023-03-18 13:28:12 +0200

    added notice model

--- models/index.js
+++ models/index.js
@@ -1,7 +1,9 @@
 const { News } = require('./news');
-const User  = require('./user');
+const User = require('./user');
+const Notice = require('./notice');
 
 module.exports = {
   News,
   User,
+  Notice,
 };
