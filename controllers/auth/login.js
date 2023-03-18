36f1811f1bf6611880235eed71132d3deacb6832
AVViktoria <56619435+AVViktoria@users.noreply.github.com>
2023-03-18 13:02:03 -0700

    feat: create login

--- controllers/auth/login.js
+++ controllers/auth/login.js
@@ -1,5 +1,29 @@
-const login = async (req, res, next) => {
-  //   
+const bcrypt = require('bcrypt');
+const { findUserByEmail, loginUser } = require('../../services/authService');
+const { generateToken } = require('../../helpers/generateToken');
 
-}
-  module.exports = login;
+const login = async (req, res) => {
+  const { email, password } = req.body;
+  const user = await findUserByEmail({ email });
+
+  if (!user) {
+    return res.status(409).json({ message: 'User with this email not found' });
+  }
+
+  const isPasswordValid = await bcrypt.compare(password, user.password);
+  if (!isPasswordValid) {
+    return res.status(401).json({ message: `Email or password is wrong` });
+  }
+  
+  const token = await generateToken(user);
+  await loginUser(user._id, token);
+
+  res.json({
+    status: 'success',
+    code: 200,
+    token: token,
+    user: { email: user.email, name: user.name },
+  });
+};
+
+module.exports = login;
---
+++ helpers/index.js
@@ -0,0 +1,3 @@
+const { generateToken } = require('./generateToken');
+
+module.exports = { generateToken };
