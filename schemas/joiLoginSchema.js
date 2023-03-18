ba4e32eb8676236055c3124452b2b9b85db33360
AVViktoria <56619435+AVViktoria@users.noreply.github.com>
2023-03-17 18:04:10 -0700

    fix: name  joiLoginSchema

--- schemas/joiLoginSchema.js
+++ schemas/joiLoginSchema.js
@@ -2,7 +2,7 @@ const Joi = require('joi');
 
 const { emailPattern, passwordPattern } = require('./patterns');
 
-const joiSignupSchema = Joi.object({
+const joiLoginSchema = Joi.object({
   email: Joi.string().pattern(emailPattern).min(10).max(40).required(),
   password: Joi.string().pattern(passwordPattern).required(),
  
