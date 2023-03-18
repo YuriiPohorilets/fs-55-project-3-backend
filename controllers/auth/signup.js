249b0791e1926823528bfa5579f338bd5d3a49a1
AVViktoria <56619435+AVViktoria@users.noreply.github.com>
2023-03-17 23:11:29 -0700

    feat: start create auth/signup controller

--- controllers/auth/signup.js
+++ controllers/auth/signup.js
@@ -1,26 +1,35 @@
-// const bcrypt = require("bcryptjs");
+const bcrypt = require("bcryptjs");
 // const { SECRET_KEY } = process.env;
-// const { User } = require("../../models");
+const { User } = require("../../models/index");
 
 const signup = async (req, res, next) => {
-//   try {
-//     const { email, password } = req.body;
-//     const user = await User.findOne({ email });
+  const { name, email, password } = req.body;
 
-//     if (user) {
-//       throw new Conflict(`User with ${email} already exist `);
-//     }
-// const hashPassword = bcrypt.hashSync(password, bcrypt.genSaltSync(10));
+  try {
+   const user = await User.findOne({ email });
 
-//     const result = await User.create({ email, password: hashPassword });
-//     res.status(201).json({
-//       status: "success", 
-//       code: 201,
-//       user: { email: result.email, subscription: result.subscription },
-//     });
-//   } catch (error) {
-//     next(error);
-//   }
+    if (user) {
+      res.status(409).json({ message: `User with ${email} already exist ` });
+    return;
+    }
+    if (!email || !password || !name) {
+      res.status(400).json({ message: 'Name, email or password not found' });
+      return;
+    }
+const hashPassword = bcrypt.hashSync(password, bcrypt.genSaltSync(10));
+
+    const result = await User.create({name,  email, password: hashPassword });
+
+
+    
+    res.status(201).json({
+      status: "success", 
+      code: 201,
+      user: { name: result.name, email: result.email},
+    });
+  } catch (error) {
+    next(error);
+  }
 };
 
 module.exports = signup;
--- models/user.js
+++ models/user.js
@@ -47,13 +47,13 @@ const userSchema = new Schema(
   { versionKey: false, timestamps: true }
 );
 
-userSchema.methods.setPassword = function (password) {
-  this.password = bcrypt.hashSync(password, bcrypt.genSaltSync(10));
-};
+// userSchema.methods.setPassword = function (password) {
+//   this.password = bcrypt.hashSync(password, bcrypt.genSaltSync(10));
+// };
 userSchema.methods.comparePassword = function (password) {
   return bcrypt.compareSync(password, this.password);
 };
 
 const User = model('user', userSchema);
 
-module.exports = User;
+module.exports = {User};
