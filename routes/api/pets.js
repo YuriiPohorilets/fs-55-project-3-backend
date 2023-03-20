9e7c43b112c204dcace7258ab0ab526e5085ae80
YuriiPohorilets <pohorilets.y@gmail.com>
2023-03-20 17:25:46 +0200

    created upload cloudinary middleware

--- routes/api/pets.js
+++ routes/api/pets.js
@@ -1,12 +1,18 @@
 const express = require('express');
 const router = express.Router();
 const { pets: ctrl } = require('../../controllers');
-const { auth } = require('../../middleware');
+const { auth, upload } = require('../../middleware');
 
 //================ CREATE NEW PET ================
 router.post('/', auth, ctrl.createPet);
 
 //================ DELETE PET BY ID ================
 router.delete('/:petId', auth, ctrl.deletePet);
+
+//================ CLOUDINARY TEST ================
+// router.post('/upload', upload.single('image'), async (req, res) => {
+//   const imagePath = req.file.path;
+//   console.log(imagePath);
+// });
 
 module.exports = router;
