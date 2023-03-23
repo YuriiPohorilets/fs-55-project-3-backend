fe2e35155bd8ad3d3ab5abae5d5c429cf73b3689
AVViktoria <56619435+AVViktoria@users.noreply.github.com>
2023-03-22 19:28:39 -0700

    fix: regex for name, passw, email

--- controllers/auth/refresh.js
+++ controllers/auth/refresh.js
@@ -1,9 +1,15 @@
-const refresh = async (req, res) => {
+const { findUserByEmail } = require('../../services/authService');
+const asyncHandler = require('express-async-handler');
+
+const refresh = asyncHandler(async (req, res) => {
   const { refreshToken } = req.cookies;
 
   if (!refreshToken) {
     return res.status(401).json({ message: 'Not authorized' });
   }
+  const { email } = req.body;
+  const user = await findUserByEmail({ email });
+
   res.cookie('refreshToken', refreshToken, {
     httpOnly: true,
     maxAge: 30 * 24 * 60 * 60 * 1000,
@@ -12,18 +18,17 @@ const refresh = async (req, res) => {
   res.json({
     status: 'success',
     code: 200,
-    // token: token,
-    refreshToken: refreshToken,
-    // result: {
-    //   name: user.name,
-    //   email: user.email,
-    //   phone: user.phone,
-    //   city: user.city,
-    //   birthday: user.birthday,
-    //   avatarURL: user.avatarURL,
-    //   favorite: user.favorite,
-    // },
+    refreshToken,
+    result: {
+      name: user.name,
+      email: user.email,
+      phone: user.phone,
+      city: user.city,
+      birthday: user.birthday,
+      avatarURL: user.avatarURL,
+      favorite: user.favorite,
+    },
   });
-};
+});
 
 module.exports = refresh;
--- schemas/joiLoginSchema.js
+++ schemas/joiLoginSchema.js
@@ -3,8 +3,8 @@ const Joi = require('joi');
 const { emailPattern, passwordPattern } = require('./patterns');
 
 const joiLoginSchema = Joi.object({
-  email: Joi.string().pattern(emailPattern).min(10).max(40).required(),
-  password: Joi.string().pattern(passwordPattern).required(),
+  email: Joi.string().pattern(emailPattern).min(10).max(64).required(),
+  password: Joi.string().pattern(passwordPattern).min(7).max(32).required(),
 });
 
 module.exports = joiLoginSchema;
