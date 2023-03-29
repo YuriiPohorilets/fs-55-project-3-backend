9fd38459bc74795336a2c8702e8d3bbbd6d6b993
YuriiPohorilets <pohorilets.y@gmail.com>
2023-03-29 11:41:49 +0300

    added sort pets

--- models/pet.js
+++ models/pet.js
@@ -1,31 +1,34 @@
 const { model, Schema } = require('mongoose');
 
-const petSchema = new Schema({
-  name: {
-    type: String,
-    required: [true, 'Field is required'],
+const petSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: [true, 'Field is required'],
+    },
+    birthday: {
+      type: String,
+      required: [true, 'Field is required'],
+    },
+    breed: {
+      type: String,
+      required: [true, 'Field is required'],
+    },
+    comment: {
+      type: String,
+      required: [true, 'Field is required'],
+    },
+    photo: {
+      type: String,
+    },
+    owner: {
+      type: Schema.Types.ObjectId,
+      ref: 'user',
+      required: true,
+    },
   },
-  birthday: {
-    type: String,
-    required: [true, 'Field is required'],
-  },
-  breed: {
-    type: String,
-    required: [true, 'Field is required'],
-  },
-  comment: {
-    type: String,
-    required: [true, 'Field is required'],
-  },
-  photo: {
-    type: String,
-  },
-  owner: {
-    type: Schema.Types.ObjectId,
-    ref: 'user',
-    required: true,
-  },
-});
+  { versionKey: false, timestamps: true }
+);
 
 const Pet = model('pets', petSchema);
 
--- services/userPetsService.js
+++ services/userPetsService.js
@@ -1,7 +1,7 @@
 const { Pet } = require('../models');
 
 const findUserPets = async _id => {
-  const pets = await Pet.find({ owner: _id });
+  const pets = await Pet.find({ owner: _id }).sort({ createdAt: -1 });
 
   return pets;
 };
